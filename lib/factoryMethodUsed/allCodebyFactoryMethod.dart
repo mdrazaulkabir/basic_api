eb7a0c0ec56f18cab7984cdc5cda79fa374d09d8
mdrazaulkabir <razaulkabirit@gmail.com>
2025-11-06 19:08:21 +0600

    added futureBuilder and skip initState

--- lib/factoryMethodUsed/allCodebyFactoryMethod.dart
+++ lib/factoryMethodUsed/allCodebyFactoryMethod.dart
@@ -1,6 +1,5 @@
 
 import 'package:basic_api/factoryMethodUsed/api_services.dart';
-import 'package:basic_api/factoryMethodUsed/postModel.dart';
 import 'package:flutter/material.dart';
 
 class Allcodebyfactorymethod extends StatefulWidget {
@@ -11,26 +10,22 @@ class Allcodebyfactorymethod extends StatefulWidget {
 }
 
 class _AllcodebyfactorymethodState extends State<Allcodebyfactorymethod> {
-  List<PostModel>posts = [];
 
-
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    fetchData();
-  }
-  fetchData() async{
-    final data=await ApiServices.fetchData();
-    print(data?.length);
-
-    posts=data!;
-
-    // setState(() {
-    //   posts=data!;
-    // });
-  }
+  // List<PostModel>posts = [];
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   fetchData();
+  // }
+  // fetchData() async{
+  //   final data=await ApiServices.fetchData();
+  //   print(data?.length);
+  //   posts=data!;
+  //   // setState(() {
+  //   //   posts=data!;
+  //   // });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -57,18 +52,28 @@ class _AllcodebyfactorymethodState extends State<Allcodebyfactorymethod> {
           ),
         ),
       ),
-      body: ListView.builder(
-        itemCount: posts.length,
-          itemBuilder: (context,index){
-          final data=posts[index];
-        return Card(
-          child: ListTile(
-            title: Text("Title:${data.title}"),
-            subtitle: Text('Description${data.description}',maxLines: 1,overflow: TextOverflow.ellipsis,),
-            leading:Image.network(data.urlToImage,height: double.maxFinite,),
-          ),
-        );
-      })
+      body: FutureBuilder(
+        future: ApiServices.fetchData(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState== ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+              itemBuilder: (context,index){
+              final data=snapshot.data![index];
+            return Card(
+              child: ListTile(
+                title: Text("Title:${data.title}"),
+                subtitle: Text('Description${data.description}',maxLines: 2,overflow: TextOverflow.ellipsis,),
+                leading:Image.network(data.urlToImage,height: double.maxFinite,),
+              ),
+            );
+          });
+        }
+      )
     );
   }
 }
